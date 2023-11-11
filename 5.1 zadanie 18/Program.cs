ad35c848705b751c263c949d20bcf9171a5d142e
Asus <Asus@LAPTOP-4QTPOURE>
2023-11-11 17:14:50 +0300

    Добавьте файлы проекта.

---
+++ 5.1 zadanie 18/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("введите строку:");
+char[] mas = Console.ReadLine().ToCharArray();
+int count = 0;
+int start = 0;
+for (int i = 0; i < mas.Length; i++)
+{
+    if (mas[i] == ';' || mas[i] == ':')
+    {
+        if (mas[start - 1] == 'а') count++;
+    }
+    start = i + 1;
+}
+Console.WriteLine("Количество слов, заканчивающихся на 'а':" + count);
